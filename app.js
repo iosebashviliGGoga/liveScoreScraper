bd26551d51ffb7c7c891f926826b726ff18b5af8
iosebashviliggoga <iosebashviliggoga@gmail.com>
2025-07-16 22:54:40 +0400

    fixed email text

--- app.js
+++ app.js
@@ -105,7 +105,7 @@ async function executeLogic() {
             newLines.push(
               `UnID - ${recId} ::: Match Id - ${m.id} ::: ${e.content} Goal Canceled @${e.status}`
             );
-            emailContent.push(recId); // Add only the recId to email content
+            emailContent.push(`${e.content} Goal Canceled @${e.status}`); // Add the descriptive part
           } else {
             console.log(`${e.content}  Goal Canceled @${e.status} -- OLD`);
           }
